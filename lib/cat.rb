bcd965ab9f5368659b5aceb8c8718f7506fdc304
nicolewang2000 <nicole.wang@yale.edu>
2020-05-28 14:49:32 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 class Cat
-  # code goes here
+  attr_reader :name, :mood
+  attr_accessor :owner
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end  
+
+  def mood=(mood)
+    @mood = mood
+  end 
+
+  def self.all
+    @@all
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,63 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize (name, species = 'human')
+    @name = name
+    @species = species
+    @@all << self
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.collect{|owner| owner.name}.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end
+
+  def buy_cat(new_pet)
+    self.cats << Cat.new(new_pet, self)
+  end 
+
+  def buy_dog(new_pet)
+    self.dogs << Dog.new(new_pet, self)
+  end 
+
+  def walk_dogs
+    Dog.all.each{|dog| dog.mood  = "happy"}
+  end 
+
+  def feed_cats
+    Cat.all.each{|cat| cat.mood  = "happy"}
+  end
+
+  def sell_pets
+    Dog.all.each{|dog| dog.mood  = "nervous"}
+    Cat.all.each{|cat| cat.mood  = "nervous"}
+    Dog.all.each{|dog| dog.owner  = nil}
+    Cat.all.each{|cat| cat.owner  = nil}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
